5e6f9a8b389f66579a12613b0f2ad8b03d3402c4
Fisher Darling <fdarlingco@gmail.com>
2019-04-25 02:13:59 +0000

    Added mul builtin

--- src/builtins.rs
+++ src/builtins.rs
@@ -7,6 +7,8 @@ pub mod math {
 
     pub fn add() -> (String, BuiltinFunction) {
         let fun = |args: Vector<Type>, _scope: &mut Scope| -> Type {
+            // println!("add call: {:?}", args);
+            
             let mut sum_int: i64 = 0;
             let mut sum_float: f64 = 0.0;
 
@@ -26,5 +28,30 @@ pub mod math {
         };
 
         ("add".into(), BuiltinFunction::new("add".into(), fun))
+    }
+
+    pub fn mul() -> (String, BuiltinFunction) {
+        let fun = |args: Vector<Type>, _scope: &mut Scope| -> Type {
+            // println!("mul call: {:?}", args);
+            
+            let mut mul_int: i64 = 1;
+            let mut mul_float: f64 = 1.0;
+
+            for value in args {
+                match value {
+                    Type::Int(i) => mul_int *= i,
+                    Type::Float(f) => mul_float *= f,
+                    t => panic!("invalid argument for `mul`: {:?}", t),
+                }
+            }
+
+            if mul_float > 1.0 {
+                Type::Float(mul_int as f64 * mul_float)
+            } else {
+                Type::Int(mul_int)
+            }
+        };
+
+        ("mul".into(), BuiltinFunction::new("mul".into(), fun))
     }
 }
